bd3642e56ced0147baa3fe9004732f5233c53658
Utsabbasyal12 <utsabbasyal12@gmail.com>
2021-09-13 22:30:41 +0545

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,40 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: Colors.blue,
       appBar: AppBar(
         title: Center(child: Text('Dicee')),
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blue,
       ),
+      body: DicePage(),
     ),
   ));
 }
 
 class DicePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image(
+                image: AssetImage('images/dice1.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image(
+                image: AssetImage('images/dice2.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
-
